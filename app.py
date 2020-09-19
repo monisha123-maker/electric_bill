b129311c78e04201274cdcc4a968f0826d589b2f
monisha123-maker <63732693+monisha123-maker@users.noreply.github.com>
2020-09-19 04:46:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,32 +15,90 @@ def main():
 def send():
     if request.method == 'POST':
         # start pulling out data from input form input
+        name = request.form['name']
+        MD = float(request.form['MD'])
         num1 = request.form['num1']
         num2 = request.form['num2']
         num3 = request.form['num3']
+        num4 = float(request.form['num4'])
+        Solar = request.form['Solar']
+        yes = 'yes'
+        no = 'no'
     #calculation IF statement  
         units = float(num3)-float(num2)
-        if units<=100:
-            surcharge=25
-            amount = float(units*0.90)
-            net_amount = amount+surcharge
-            return render_template('app.html',amount=amount , surcharge=surcharge,net_amount=net_amount )
-        elif units>100 and units<=300:
-            surcharge=35
-            amount = float(units*1.50)
-            net_amount = amount+surcharge
-            return render_template('app.html',amount=amount, surcharge=surcharge,net_amount=net_amount )
-        elif units>300 and units<=500:
-            surcharge=45
-            amount=float(units*2.75)
-            net_amount = amount+surcharge
-            return render_template('app.html',amount=amount,surcharge=surcharge, net_amount=net_amount)
-        else:
-            units>500
-            surcharge=100
-            amount=float(units*4.50)
-            net_amount = amount+surcharge
-            return render_template('app.html',amount=amount ,surcharge=surcharge, net_amount=net_amount)
+        if units==0 :
+            total_amount = 'nil'
+            if MD <=num4:
+                G = 'Great! , You have no fine'
+            elif MD > num4:
+                G = 'oops! , you are charged fine '
+                num4 = float(MD)
+            if num4==1:
+                sanction_load = float(1*60)
+            elif num4>1:
+                sanction_load = float(1*60)
+                next_sanction_charge = float(num4-1)
+                sanction_load2 = float(next_sanction_charge*70)
+                total_sanction_load = float(sanction_load+sanction_load2)
+            net_amount = total_sanction_load+1
+            if Solar==yes:
+                s = 'Great! , you have a discount for installation of solar'
+                bill = float(net_amount-31.06)
+            elif Solar==no:
+                s = 'Sorry! , you have no discount'
+                bill = float(net_amount)
+            return render_template('app.html',G=G,name=name,units=units,total_amount=total_amount,total_sanction_load=total_sanction_load,bill=bill,s=s)
+
+        if units<=30 :
+            total_amount = float(30*3.7)
+            if MD <=num4:
+                G = 'Great! , You have no fine'
+            elif MD > num4:
+                G = 'oops! , you are charged fine '
+                num4 = float(MD)
+            if num4==1:
+                sanction_load = float(1*60)
+            elif num4>1:
+                sanction_load = float(1*60)
+                next_sanction_charge = float(num4-1)
+                sanction_load2 = float(next_sanction_charge*70)
+                total_sanction_load = float(sanction_load+sanction_load2)
+            net_amount = total_amount+total_sanction_load+1
+            if Solar==yes:
+                s = 'Great! , you have a discount for installation of solar'
+                bill = float(net_amount-31.06)
+            elif Solar==no:
+                s = 'Sorry! , you have no discount'
+                bill = float(net_amount)
+            return render_template('app.html',G=G,name=name,units=units,total_amount=total_amount,total_sanction_load=total_sanction_load,bill=bill,s=s)
+            
+        if units>30 :
+            amount = float(30*3.7)
+            next_amount = float(units-30)
+            amount2 = float(next_amount*5.2)
+            total_amount = float(amount+amount2)
+            if MD <=num4:
+                G = 'Great! , You have no fine'
+            elif MD > num4:
+                G = 'oops! , you are charged fine'
+                num4 = float(MD)
+            if num4==1:
+                sanction_load = float(1*60)
+            elif num4>1:
+                sanction_load = float(1*60)
+                next_sanction_charge = float(num4-1)
+                sanction_load2 = float(next_sanction_charge*70)
+                total_sanction_load = float(sanction_load+sanction_load2)
+            
+            net_amount = total_amount+total_sanction_load+1
+
+            if Solar==yes:
+                s = 'Great! , you have a discount for installation of solar'
+                bill = float(net_amount-31.06)
+            elif Solar==no:
+                s = 'Sorry! , you have no discount'
+                bill = float(net_amount)
+            return render_template('app.html',G=G,name=name,units=units,total_amount=total_amount,total_sanction_load=total_sanction_load,bill=bill,s=s)
 
         
 
